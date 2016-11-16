7ab37b0519583c0863d5184ef1dd50b5888f33ef
Brad Copenhaver and Ryan Mathisen <brad+ryan@>
2016-11-16 09:07:58 -0800

    add initial files and folders

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using ToDoList.Objects;
+using System.Collections.Generic;
+
+namespace EnterNamespaceHere //replace with your desired namespace
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      //Insert your GETs and POSTs here
+    }
+  }
+}
